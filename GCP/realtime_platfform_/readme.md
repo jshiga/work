e79f007e2a7b82c9355ff7df345c9b343cfe5989
j_shiga <shiga@eight-hundred.com>
2023-10-19 11:48:56 +0900

    20231019_commit

---
+++ GCP/realtime_platfform_/readme.md
@@ -0,0 +1,10 @@
+### how to use ngrok
+
+> python3 -m http.server 80
+
+> ngrok http 80
+
+* 一度だけ実行
+> ngrok config add-authtoken XXXXXXXX
+
+### 構成図
---
+++ GCP/realtime_platfform_/src/SSGTM/from_client_to_CloudFunctions_via_SSGtm.js
@@ -0,0 +1,27 @@
+const getAllEventData = require('getAllEventData');
+const log = require("logToConsole");
+const JSON = require("JSON");
+const sendHttpRequest = require('sendHttpRequest');
+
+// log(data);
+
+const postBody = JSON.stringify(getAllEventData());
+
+log('postBody parsed to:', postBody);
+
+// CloudFunctionsのエンドポイントURLを取得する
+const url = data.endpoint;
+
+log('Sending event data to:' + url);
+
+const options = {method: 'POST', 
+                 headers: {'Content-Type':'application/json'}};
+
+// Sends a POST request
+sendHttpRequest(url, (statusCode) => {
+  if (statusCode >= 200 && statusCode < 300) {
+    data.gtmOnSuccess();
+  } else {
+    data.gtmOnFailure();
+  }
+}, options, postBody);
